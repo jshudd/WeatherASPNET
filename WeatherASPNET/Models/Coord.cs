a6b372603bb248015ee66a5ec08ac3acb7c06e4e
Jeremy Huddleston <jshudd@gmail.com>
2021-12-08 10:44:44 -0600

    Create Coord.cs
    created Coord struct for lat & long

---
+++ WeatherASPNET/Models/Coord.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace WeatherASPNET.Models
+{
+    public struct Coord
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
