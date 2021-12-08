4f95b7f0918d1701eb533dd564f6119c8b7e72a6
Jeremy Huddleston <jshudd@gmail.com>
2021-12-08 10:46:19 -0600

    Create Weather.cs
    created Weather class; added model to Index

---
+++ WeatherASPNET/Models/Weather.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace WeatherASPNET.Models
+{
+    public class Weather
+    {
+        public Weather()
+        {
+        }
+
+        public int ZipCode { get; set; }
+        public Coord Coordinate { get; set; }
+        public string WxMainDesc { get; set; }
+        public string WxDescription { get; set; }
+        public double Temp { get; set; }
+        public double FeelsLike { get; set; }
+        public double TempMax { get; set; }
+        public double TempMin { get; set; }
+        public double Pressure { get; set; }
+        public int Humidity { get; set; }
+        public int Visibility { get; set; }
+        public double WindSpeed { get; set; }
+        public double WindDeg { get; set; }
+        public double WindGust { get; set; }
+        public string City { get; set; }
+    }
+}
