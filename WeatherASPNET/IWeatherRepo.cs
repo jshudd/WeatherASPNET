3957f88b67535a145f33838a4f8dd9f5f23c6808
Jeremy Huddleston <jshudd@gmail.com>
2021-12-08 11:25:48 -0600

    Create IWeatherRepo.cs
    set up interface

---
+++ WeatherASPNET/IWeatherRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using WeatherASPNET.Models;
+
+namespace WeatherASPNET
+{
+    public interface IWeatherRepo
+    {
+        public string GetAPIResponse(string zipCode);
+
+        public Weather ParseResponse(string response);
+    }
+}
